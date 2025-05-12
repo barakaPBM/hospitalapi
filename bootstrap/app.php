9cb7d385f497e9686d097befc19eec660b91b5c4
baraka <kalingabaraka@gmail.com>
2025-05-12 15:15:06 +0300

    forced json response in middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,29 +4,33 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->api([
-         
+
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-           
-         
+
+
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-        $exceptions->render(function (AuthenticationException $e, $request) {
-            return response()->json([
-                'message' => 'Unauthenticated.'
-            ], 401);
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            if ($request->is('api/*')) {
+                return true;
+            }
+
+            return $request->expectsJson();
         });
     })->create();
